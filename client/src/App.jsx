e80c9975cdb995be879d64438365f43d8c4c8c54
Akshat Kumar <akshatkumar2022@vitbhopal.ac.in>
2025-10-29 19:57:08 +0530

    build login ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,3 +29,15 @@ const App = () => {
 };
 
 export default App;
+
+const Root = () => {
+  // Check if token exists in localstorage:
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // redirect to dashboard if authentiated else to login
+  return isAuthenticated ? (
+    <Navigate to={"/dashboard"} />
+  ) : (
+    <Navigate to={"/login"} />
+  );
+};
--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default Login;
